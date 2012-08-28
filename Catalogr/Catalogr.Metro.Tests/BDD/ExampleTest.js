3c2b252938910df81168bc067d6f53f57d43895f
Artur Herczeg <artur.herczeg@gmail.com>
2012-08-28 11:48:44 +0200

    Mocha JS BDD test framework integration

---
+++ Catalogr/Catalogr.Metro.Tests/BDD/ExampleTest.js
@@ -0,0 +1,7 @@
+﻿describe("Test object", function() {
+    describe("Subobject", function () {
+        it("test case", function() {
+            expect(1 + 2).to.equal(3);
+        });
+    });
+});
---
+++ Catalogr/Catalogr.Metro.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Catalogr.Metro.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
