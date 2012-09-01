751616714097a78267cb618e4e6b5fb28c14b146
Artur Herczeg <artur.herczeg@gmail.com>
2012-09-01 19:43:49 +0200

    Logger extended with std console methods

--- Catalogr/Catalogr.Metro/js/Infra/logger.js
+++ Catalogr/Catalogr.Metro/js/Infra/logger.js
@@ -1,17 +1,40 @@
 ﻿define(function () {
     "use strict";
 
+    function argToArray(arg) {
+        var i, len,
+                args = [];
+
+        for (i = 0, len = arg.length; i < len; i++) {
+            args.push(arg[i]);
+        }
+
+        return args;
+    }
 
     return {
         log: function() {
-            var i,
-                a = [];
+            var args = argToArray(arguments);
 
-            for (i = 0; i < arguments.length; i++) {
-                a.push(arguments[i]);
-            }
+            console.log(args.join(" "));
+        },
+        error: function () {
+            var args = argToArray(arguments);
 
-            console.log(a.join(" "));
+            console.error(args.join(" "));
+        },
+        info: function () {
+            var args = argToArray(arguments);
+
+            console.info(args.join(" "));
+        },
+        warn: function () {
+            var args = argToArray(arguments);
+
+            console.warn(args.join(" "));
+        },
+        assert: function () {
+            console.assert.apply(arguments);
         }
     };
 });
