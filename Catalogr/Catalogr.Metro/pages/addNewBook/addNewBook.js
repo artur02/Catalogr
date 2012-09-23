6820b102d7d711ed1d6a1aa9bf575009dae2abd4
Artur Herczeg <artur.herczeg@gmail.com>
2012-09-23 19:15:25 +0200

    addNewBook author selector moved to own func

--- Catalogr/Catalogr.Metro/pages/addNewBook/addNewBook.js
+++ Catalogr/Catalogr.Metro/pages/addNewBook/addNewBook.js
@@ -44,50 +44,44 @@
         });
     }
 
-        WinJS.UI.Pages.define("/pages/addNewBook/addNewBook.html", {
-            // This function is called whenever a user navigates to this page. It
-            // populates the page elements with the app's data.
-            ready: function (element, options) {
-                // TODO: Initialize the page here.
-                
-                
-                    require(["data", '/js/Infra/templateLoader.js'], function (data, loader) {
-                    loader.load('pages\\addNewBook\\templates').done(function () {
-                        var view = Ember.View.create({
-                            templateName: 'authors',
-                            options: []
-                        });
+    function addAuthorSelector() {
+        require(["data", '/js/Infra/templateLoader.js'], function(data, loader) {
+            loader.load('pages\\addNewBook\\templates').done(function() {
+                var view = Ember.View.create({
+                    templateName: 'authors',
+                    options: []
+                });
 
-                        data.authors().forEach(function(author, index) {
-                            view.options.push({
-                                text: author,
-                                value: index
-                            });
-                        });
-                        
-                        view.appendTo('#selectauthor');
-
-                        addMetaCommandHandler();
-                        addSaveCommandHandler();
-                        
-
-
-                    }, function (err) {
-                        debugger;
+                data.authors().forEach(function(author, index) {
+                    view.options.push({
+                        text: author,
+                        value: index
                     });
                 });
 
-                
-            },
-
-            unload: function () {
-                // TODO: Respond to navigations away from this page.
-            },
-
-            updateLayout: function (element, viewState, lastViewState) {
-                /// <param name="element" domElement="true" />
-
-                // TODO: Respond to changes in viewState.
-            }
+                view.appendTo('#selectauthor');
+            });
         });
+    }
+
+    WinJS.UI.Pages.define("/pages/addNewBook/addNewBook.html", {
+        ready: function(element, options) {
+            // TODO: Initialize the page here.
+           
+                    addAuthorSelector();
+
+                    addMetaCommandHandler();
+                    addSaveCommandHandler();                        
+        },
+
+        unload: function() {
+            // TODO: Respond to navigations away from this page.
+        },
+
+        updateLayout: function(element, viewState, lastViewState) {
+            /// <param name="element" domElement="true" />
+
+            // TODO: Respond to changes in viewState.
+        }
+    });
 }());
